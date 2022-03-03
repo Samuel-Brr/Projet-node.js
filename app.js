dad881c44a1bdc1bf60d77c5b253421e3e1c5e06
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-03 10:12:16 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false })); // Permet d'avoir accés au body de la requete via l 'url
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('621f69a8ea58a64a58ccea72')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    "mongodb+srv://Sml-Brr:YVmG4mNq24wxbku2@cluster0.jt4fy.mongodb.net/shop?retryWrites=true&w=majority",{ useNewUrlParser: true,
+    useUnifiedTopology: true }, 
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if(!user){
+        const user = new User({
+          name: 'Sam',
+          email: 'sam@test.com',
+          cart: {
+            items: []
+          }
+        })
+        user.save()  
+      }
+    })
+    app.listen(3000, ()=>{
+      console.log("listening on port 3000...")
+    });
+    console.log("connected to database !")
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
