7bf6125ac0817cf528c5883cfc84210595184e91
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-04 01:10:35 +0100

    257 Adding Login Functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,19 +19,39 @@ exports.getLogin = (req, res, next) => {
     });
   };
 
-  exports.postLogin = (req, res, next) => {
-    User.findById('621f69a8ea58a64a58ccea72')
-    .then(user => {
-      req.session.isLoggedIn = true
-      req.session.user = user;
-      req.session.save(err=>{
-        console.log(err)
-        res.redirect("/")
-      })
-    })
-    .catch(err => console.log(err));
-};
+  // bcrypt.compare retourne une promesse, le bloc "then" ne signifie pas que les mots de passe correspondent 
+  // mais juste qu'il n y a pas eu d'erreur dans le processus de vérification
+  // le bloc "then" retourne un boolean (doMatch) qui sera false si les mdps ne correspondent pas ou true si ils correspondent
 
+  exports.postLogin = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({ email: email })
+      .then(user => {
+        if (!user) {
+          return res.redirect('/login');
+        }
+        bcrypt
+          .compare(password, user.password)
+          .then(doMatch => {
+            if (doMatch) {
+              req.session.isLoggedIn = true;
+              req.session.user = user;
+              return req.session.save(err => {
+                console.log(err);
+                res.redirect('/');
+              });
+            }
+            res.redirect('/login');
+          })
+          .catch(err => {
+            console.log(err);
+            res.redirect('/login');
+          });
+      })
+      .catch(err => console.log(err));
+  };
+  
 exports.postSignup = (req, res, next) => {
   const email = req.body.email
   const password = req.body.password
@@ -41,18 +61,19 @@ exports.postSignup = (req, res, next) => {
       if(userDoc){
         return res.redirect('/signup')
       }
-      return bcrypt.hash(password, 12)
-    })
-    .then(hashedPassword => {
-      const user = new User({
-        email:email,
-        password:hashedPassword,
-        cart: { items: [] }
-      })
-      return user.save()
-    })
-    .then(result=>{
-      res.redirect('/login')
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email:email,
+            password:hashedPassword,
+            cart: { items: [] }
+          })
+          return user.save()
+        })
+        .then(result=>{
+          res.redirect('/login')
+        })
     })
     .catch(err => {
     console.log(err)
