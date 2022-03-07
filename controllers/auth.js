37af1a6d83bed2a4a0b32282a39bcfe92eba1b16
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-07 12:44:16 +0100

    296 adding async validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs')
 // const sendgridTransport = require('nodemailer-sendgrid-transport')
 
 const User = require("../models/user")
-const {validationResult} = require('express-validator/check')
+const {validationResult} = require('express-validator')
 
 // const transporter = nodemailer.createTransport(sendgridTransport({
 //   auth:{
@@ -78,7 +78,6 @@ exports.getSignup = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email
   const password = req.body.password
-  const confirmPassword = req.body.confirmPassword
   const errors = validationResult(req)
   if(!errors.isEmpty()){
     return res.status(422).render(res.render('auth/signup', {
@@ -87,38 +86,29 @@ exports.postSignup = (req, res, next) => {
       errorMessage: errors.array()[0].msg
     }))
   }
-  User.findOne({email: email})
-    .then(userDoc => {
-      if(userDoc){
-        req.flash('error', 'E-mail exists already, please pick a different one')
-        return res.redirect('/signup')
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then(hashedPassword => {
-          const user = new User({
-            email:email,
-            password:hashedPassword,
-            cart: { items: [] }
-          })
-          return user.save()
-        })
-        .then(result=>{
-          res.redirect('/login')
-          // transporter.sendMail({
-          //   to: email,
-          //   from: 'shop@node-complete.com',
-          //   subject: 'Signup succeeded !',
-          //   html: '<h1>You successfully signed up !</h1>'
-          // })
-        })
-        .catch(err=>{
-          console.log(err)
-        })
+
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email:email,
+        password:hashedPassword,
+        cart: { items: [] }
+      })
+      return user.save()
     })
-    .catch(err => {
-    console.log(err)
-  })
+    .then(result=>{
+      res.redirect('/login')
+      // transporter.sendMail({
+      //   to: email,
+      //   from: 'shop@node-complete.com',
+      //   subject: 'Signup succeeded !',
+      //   html: '<h1>You successfully signed up !</h1>'
+      // })
+    })
+    .catch(err=>{
+      console.log(err)
+    })
 };
 
 exports.postLogout = (req, res, next) => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const {check} = require('express-validator/check')
+const {check} = require('express-validator')
+const User = require("../models/user")
 
-const authController = require('../controllers/auth')
+const authController = require('../controllers/auth');
+const { promiseImpl } = require('ejs');
 
 const router = express.Router();
 
@@ -11,10 +13,40 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin)
 
-router.post('/signup', 
-    check('email').isEmail().withMessage("please entera valid email"),
-    body('password','plz enter a pswrd with at least 5 alphanumeric char').isLength({min:5}).isAlphanumeric(), 
-    authController.postSignup);
+router.post(
+    '/signup',
+    [
+      check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email.')
+        .custom((value, { req }) => {
+          // if (value === 'test@test.com') {
+          //   throw new Error('This email address if forbidden.');
+          // }
+          // return true;
+          return User.findOne({ email: value }).then(userDoc => {
+            if (userDoc) {
+              return Promise.reject(
+                'E-Mail exists already, please pick a different one.'
+              );
+            }
+          });
+        }),
+      check(
+        'password',
+        'Please enter a password with only numbers and text and at least 5 characters.'
+      )
+        .isLength({ min: 5 })
+        .isAlphanumeric(),
+      check('confirmPassword').custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match!');
+        }
+        return true;
+      })
+    ],
+    authController.postSignup
+  );
 
 router.post('/logout', authController.postLogout)
 
